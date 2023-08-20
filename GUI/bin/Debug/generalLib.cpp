d5afacce017708c3e2a6d5ed0ebf50c190fae0a9
Fidisk <70708171+Fidisk@users.noreply.github.com>
2023-08-21 00:58:01 +0700

    No more exe
    Burn you fucker

--- GUI/bin/Debug/generalLib.cpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <bits/stdc++.h>
-#include "generalLib.h"
-#include "globalVariableController.h"
-
-//Note: This should cover all the stuff that other module share, i.e: Variable
-
-
